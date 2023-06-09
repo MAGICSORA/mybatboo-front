a45da294dc009851a7a4cf5576d808f81ee423c6
허정민 <heowjdals981227@gmail.com>
2023-06-09 14:35:14 +0900

    refactor: small fixes

--- src/module/main-layout/component/main-layout/tab-bar-layout.component.ts
+++ src/module/main-layout/component/main-layout/tab-bar-layout.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { SyntaxSharedModule } from "../../../shared/syntax-shared.module";
 import { BottomTabBarComponent } from "../../../../component/bottom-tab-bar/bottom-tab-bar.component";
 import { AppHeaderComponent } from "../../../../component/app-header/app-header.component";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { matchMedia$ } from "../../../../util/util";
+import { tap } from "rxjs";
+import { Store } from "@ngrx/store";
+import { GlobalState } from "../../../../ngrx";
+import { selectCurrentUser } from "../../../../ngrx/user.state";
+import { filter } from "rxjs/operators";
 
 @Component({
   selector: 'app-main-layout',
@@ -17,9 +22,31 @@ import { matchMedia$ } from "../../../../util/util";
   styleUrls: [ './tab-bar-layout.component.scss' ]
 })
 export class TabBarLayoutComponent implements OnInit {
-  screenSize$ = matchMedia$<number>({ xs: 1, sm: 2, md: 3, lg: 4 }, 5);
+  screenSize$ = matchMedia$<number>({ xs: 1, sm: 2, md: 3, lg: 4 }, 5).pipe(
+    tap(media => {
+      if (media > 3) {
+        this.store.select(selectCurrentUser).pipe(
+          filter(user => !!user),
+          tap(user => {
+            switch (user.authLevel) {
+              case 1:
+                break;
+              case 2:
+                this.router.navigate(['/expert'])
+                break;
+              case 3:
+                this.router.navigate(['/admin'])
+                break;
+            }
+          })
+        ).subscribe()
+      }
+    })
+  );
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private store: Store<GlobalState>,
+    private router: Router,
   ) {
   }
 
